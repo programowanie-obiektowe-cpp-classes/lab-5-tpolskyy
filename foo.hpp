6a123cad3da55b1ebf92dc00e10597a528682b4a
Tomasz Polski <tomaszpolski21@gmail.com>
2024-11-24 19:31:17 +0100

    modified:   foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
+#include <algorithm>
 
-std::vector< char > foo(std::list< Human >& people)
-{
-    // Twoja implementacja tutaj
-    return {};
+std::vector<char> foo(std::list<Human>& people) {
+    std::vector<char> result;
+
+   
+    for (auto& person : people) {
+        person.birthday();
+        result.push_back(person.isMonster() ? 'n' : 'y');
+    }
+
+    
+    std::reverse(result.begin(), result.end());
+
+    return result;
 }
+
